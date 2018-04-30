7326442e7808ac4ffcb618c6b7340bf997ad585d
Daniel Nanetti-Palacios <danielnp525@gmail.com>
2018-04-30 12:29:54 -0400

    Finished challenge

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,17 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView storyTextView;
+    Button buttonTop;
+    Button buttonBottom;
+    int mStoryIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +21,59 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
-
+        storyTextView = (TextView) findViewById(R.id.storyTextView);
+        buttonTop = (Button) findViewById(R.id.buttonTop);
+        buttonBottom = (Button) findViewById(R.id.buttonBottom);
+        mStoryIndex = 1;
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
+        buttonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1) {
+                    storyTextView.setText(R.string.T3_Story);
+                    buttonTop.setText(R.string.T3_Ans1);
+                    buttonBottom.setText(R.string.T3_Ans2);
+                    mStoryIndex = 3;
+                }
+                else if(mStoryIndex == 2) {
+                    storyTextView.setText(R.string.T3_Story);
+                    buttonTop.setText(R.string.T3_Ans1);
+                    buttonBottom.setText(R.string.T3_Ans2);
+                    mStoryIndex = 4;
+                }
+                else if(mStoryIndex == 3 || mStoryIndex == 4) {
+                    storyTextView.setText(R.string.T6_End);
+                    buttonTop.setVisibility(View.GONE);
+                    buttonBottom.setVisibility(View.GONE);
+                }
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+        buttonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1) {
+                    storyTextView.setText(R.string.T2_Story);
+                    buttonTop.setText(R.string.T2_Ans1);
+                    buttonBottom.setText(R.string.T2_Ans2);
+                    mStoryIndex = 2;
+                }
+                else if (mStoryIndex == 2) {
+                    storyTextView.setText(R.string.T4_End);
+                    buttonTop.setVisibility(View.GONE);
+                    buttonBottom.setVisibility(View.GONE);
+                }
+                else if(mStoryIndex == 3 || mStoryIndex == 4) {
+                    storyTextView.setText(R.string.T5_End);
+                    buttonTop.setVisibility(View.GONE);
+                    buttonBottom.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
 }
